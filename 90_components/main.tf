5b72585fb4d235c94684a0d8a0ca48f318ddb44a
YadavEshnithin <eshnithinyadav@gmail.com>
2025-07-08 23:41:13 +0530

    update

--- 90_components/main.tf
+++ 90_components/main.tf
@@ -1,7 +1,7 @@
 module "component" {
   for_each = var.components
   # source = "../../terraform_aws_roboshop"
-  source = "gti::https://github.com/YadavEshNithin/terraform_aws_roboshop.git?ref=main"
+  source = "git::https://github.com/YadavEshNithin/terraform_aws_roboshop.git?ref=main"
   component = each.key
   rule_priority = each.value.rule_priority
 }
