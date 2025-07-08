b9f8d2c52ac6d81b35bb64b7e2ab6f5fb02da5be
YadavEshnithin <eshnithinyadav@gmail.com>
2025-07-08 22:54:31 +0530

    update

--- 20_bastion_host/bootstrap.sh
+++ 20_bastion_host/bootstrap.sh
@@ -4,3 +4,10 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
 
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -L +20G /dev/RootVG/rootVol
+sudo lvextend -L +30G /dev/RootVG/homeVol 
+
+sudo xfs_growfs /
+
+sudo xfs_growfs /home
